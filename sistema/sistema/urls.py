a7eafd87d9fda2f92eef0e708a6bf7f21afd475b
DEVIGUANA <iguanagamer9002@gmail.com>
2026-01-15 15:23:25 +0000

    [add] fix error

--- sistema/sistema/urls.py
+++ sistema/sistema/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    path("clima/", include("clima.urls")),
+    path("clima/", include(("clima.urls", "clima"), namespace="clima")),
    
 ]
 
